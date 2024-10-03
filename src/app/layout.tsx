c2b3c038da695e3be7a1a6e7d883b3dfb31d1581
wwtfly <king_wentao@163.com>
2024-10-03 10:20:45 +0800

    stoproute init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,32 @@
+import Link from "next/link";
 import "./globals.css";
-
+import { AntdRegistry } from "@ant-design/nextjs-registry";
 export default function RootLayout({
   children,
+  team,
+  analytics,
 }: Readonly<{
   children: React.ReactNode;
+  team: React.ReactNode;
+  analytics: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <AntdRegistry>
+          <div className="container mx-auto">
+            <div className="flex justify-center text-blue-500 p-6 gap-6">
+              <Link href="/">Home</Link>
+              <Link href="/visitors">Visitors</Link>
+            </div>
+            <div className="flex gap-6">
+              {team}
+              {analytics}
+            </div>
+            {children}
+          </div>
+        </AntdRegistry>
+      </body>
     </html>
   );
 }
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Page() {
+  return (
+    <div className="flex p-6 mt-6 bg-gray-500 text-white rounded-lg ">
+      @children
+    </div>
+  );
+}
